71ee8e57adc4671aecce7e54418bbba1f171a8c5
timim02 <timim0210@gmail.com>
2025-10-24 17:32:28 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,49 @@
+import config
+print(config.token)
+from random import choice
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+API_TOKEN = config.token
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', ])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Я могу реагировать на все эти команды. /help , /info, /coin, /joke\
+""")
+
+@bot.message_handler(commands=['coin'])
+def coin_handler(message):
+    coin = choice(["ОРЕЛ", "РЕШКА"])
+    bot.reply_to(message, coin)
+
+@bot.message_handler(commands=['joke'])
+def coin_handler(message):
+    coin = choice(["Хотел как лучше — получилось как всегда.", "Мозг!, не мешай мне спать!" , "Деньги были, a тепер нету. 💸", "Что делает утка на компьютере?— Крякает код. 🦆💻"])
+    bot.reply_to(message, coin)
+
+
+# Вставь сюда токен своего бота
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
